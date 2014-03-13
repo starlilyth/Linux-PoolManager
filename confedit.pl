0f3852cbbd8ace807570b0e19f00820da91f67b8
Lily Star <lily@disorg.net>
2014-03-12 20:05:35 -0700

    refining

--- confedit.pl
+++ confedit.pl
@@ -30,22 +30,28 @@ if ($confdata ne "") {
   close $fin; 
   $confdata = ""; 
 }
-my $status = "";
+my $status = -1;
 my $ln = $in{'lname'}; if ($ln ne "") {
-$status = "Save of $savepath failed!" if (system("echo $in{'ptext'} | sudo -S -u $ln /bin/cp $tempfile $savepath")!=0);
+  $status = (system("echo $in{'ptext'} | sudo -S -u $ln /bin/cp $tempfile $savepath"));
+  $ln = ""; 
 }
-$ln = ""; unlink $tempfile; 
+my $filedata = ""; my $datafile = "";
+if ($status > 0) {
+  $datafile = $tempfile;
+} else {
+  $datafile = $savepath; 
+}
+open my $fin, '<', $datafile;
+while (<$fin>) {
+   $filedata .= "$_";
+}
+unlink $tempfile; 
 
-print "<div id='content'><table class=configeditor><tr><td class=bigger>Miner Configuration Editor</td></tr>";
-print "<tr><td class=big>Hostname: $hostname </td></tr><tr><td>Config file: $savepath ";
+print "<div id='content'><table class=configeditor><tr><td class=header>Miner Configuration Editor</td></tr>";
+print "<tr><td class=bigger>Hostname: $hostname </td></tr><tr><td>Config file: $savepath ";
 my $owner = getpwuid((stat($savepath))[4]);
 print " - Owned by: $owner";
 print "<br><small><i>Change this filepath in the <a href='config.pl'>settings</a></small></i></td></tr>";
-my $filedata = "";
-open my $fin, '<', $savepath;
-while (<$fin>) {
-  $filedata .= "$_";
-}
 print "<tr><td><form name='configedit' action='confedit.pl' method='POST'>";
 print "<textarea name='configtext' style='width:800px;height:600px'>$filedata</textarea>";
 print "</td></tr><tr><td>";
@@ -53,11 +59,11 @@ print " User: <input type='text' placeholder='username' name='lname' required>";
 print " Password: ";
 print "<input type='password' placeholder='password' name='ptext' required> ";
 print " <input type='submit' value='Save'>";
-if ($status ne "") {
-  print "</td></tr><tr><td>";
-  print "$status";
+if ($status > 0) {
+  print "</td></tr><tr><td class=error>Save of $savepath failed!";
+} elsif ($status == 0) { 
+  print "</td></tr><tr><td>$savepath saved";
 }
 print "</form></td></tr>";
-print "<tr><td>WARNING! This will NOT preserve your edits if your save fails.</td></tr>";
 print "<tr><td><a href='status.pl'>Back to node page</a></td></tr>";
 print "</table></div></body></html>";
