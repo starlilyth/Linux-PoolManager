6ea4b15df8d76f116c849aaa6f2769c0841c80c7
Lily Star <lily@disorg.net>
2014-04-01 16:26:20 -0700

    version

--- config.pl
+++ config.pl
@@ -12,7 +12,7 @@
  use YAML qw( DumpFile LoadFile );
  use CGI qw(:cgi-lib :standard);
 
-my $version = "1.3.0-pre";
+my $version = "1.3.0";
 my $conffile = "/opt/ifmi/poolmanager.conf";
 if (! -f $conffile) { 
   my $nconf = {
