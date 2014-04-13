4faeafbba232d1d4994e6ee6c490a1c57129826a
Lily Star <lily@disorg.net>
2014-04-13 16:00:44 -0700

    fixed pool aliases

--- status.pl
+++ status.pl
@@ -110,18 +110,9 @@ if (defined $geng) {
 my $npalias = $in{'npalias'};
 if (defined $npalias) {
 	my $paurl = $in{'paurl'};
-	my $acount = 0;
- 	for (keys %{$conf{pools}}) {
-		if ($paurl eq ${$conf}{pools}{$_}{url}) {
-			${$conf}{pools}{$_}{alias} = $npalias;
-			$acount++;
-		}
-	}
-	if ($acount == 0) {	
-	 	my $newa = (keys %{$conf{pools}}); $newa++; 
-	 	${$conf}{pools}{$newa}{alias} = $npalias;
-	 	${$conf}{pools}{$newa}{url} = $paurl;
-	}
+	my $poolnum = $in{'poolnum'};
+	${$conf}{pools}{$poolnum}{alias} = $npalias;
+	${$conf}{pools}{$poolnum}{url} = $paurl;
 	DumpFile($conffile, $conf); 
 	$npalias = ""; $paurl = "";
 }	
@@ -775,6 +766,7 @@ if ($ispriv eq "S") {
 				$psput .= "<tr><td>Alias:</td><td>$poola</td><td colspan=2>";
 	      $psput .= "<form name='palias' action='status.pl' method='POST'>";
 				$psput .= "<input type='text' size='10' placeholder='pool alias' name='npalias'>";
+				$psput .= "<input type='hidden' name='poolnum' value='$poolnum'>";
 				$psput .= "<input type='hidden' name='paurl' value='$pname'>";
 				$psput .= "<input type='submit' value='Change'></form></td></tr>";
 			  $pusr = "unknown" if (!defined $pusr);
