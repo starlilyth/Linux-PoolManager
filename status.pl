a845c5731b04bea02ef33e873376789eb64cd3e0
Lily Star <lily@disorg.net>
2014-06-13 22:37:36 -0700

    link change for FW

--- status.pl
+++ status.pl
@@ -711,9 +711,8 @@ if (@summary) {
 				$mcontrol .= "</select></form></td>";
 			  $mcontrol .= "<td><form name='mstart' action='status.pl' method='POST'><input type='hidden' name='mstart' value='start'><input type='submit' value='Start' onclick='this.disabled=true;this.form.submit();' > </form>";
 			}
-			$mcontrol .= "</td>";		
-			my $fcheck = `ps -eo command | grep -Ec /opt/ifmi/farmview\$`;
-			$mcontrol .=  "<td><A href=/farmview.html>Farm Overview</A></td>" if ($fcheck >0);
+			$mcontrol .= "</td>";					
+			$mcontrol .=  "<td><A href=/cgi-bin/farmstatus.pl>Farm Overview</A></td>" if (-e "/usr/lib/cgi-bin/farmstatus.pl");
 			$mcontrol .=  "<td><A href=/cgi-bin/seedstatus.pl>SeedManager</A></td>" if (-e "/usr/lib/cgi-bin/seedstatus.pl");
 		}
   }
