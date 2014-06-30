1b8070c0c350a20029dd7d6c1131c11deaf5e94b
Lily Star <lily@disorg.net>
2014-06-30 14:06:06 -0700

    bounce run-poolmananger after reinstall

--- install-pm.pl
+++ install-pm.pl
@@ -1,15 +1,15 @@
 #!/usr/bin/perl
-# IFMI PoolManager installer. 
+# IFMI PoolManager installer.
 #    This file is part of IFMI PoolManager.
 #
 #    PoolManager is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.   
+#    GNU General Public License for more details.
 use strict;
 use warnings;
 use File::Path qw(make_path);
-use File::Copy; 
+use File::Copy;
 
 my $login = (getpwuid $>);
 die "Please run as root (do not use sudo)" if ($login ne 'root');
@@ -17,22 +17,22 @@ die "please execute from the install directory.\n" if (!-f "./install-pm.pl") ;
 
 if ((defined $ARGV[0]) && ($ARGV[0] eq "-q")) {
 	my $flag = $ARGV[0];
-	&doInstall($flag); 
-} else { 
+	&doInstall($flag);
+} else {
 	print "This will install the IFMI PoolManager for cgminer and clones on Linux.\n";
 	print "Are you sure? (y/n) ";
 	my $ireply = <>; chomp $ireply;
 	if ($ireply =~ m/y(es)?/i) {
 		if (-d "/var/www/IFMI/") {
 			print "It looks like this has been installed before. Do over? (y/n) ";
-			my $oreply = <>; chomp $oreply; 
+			my $oreply = <>; chomp $oreply;
 			if ($oreply =~ m/y(es)?/i) {
-				&doInstall; 
+				&doInstall;
 			} else {
 				die "Installation exited!\n";
 			}
 		} else {
-			&doInstall; 
+			&doInstall;
 		}
 	} else {
 		die "Installation exited!\n";
@@ -42,7 +42,7 @@ sub doInstall {
 	my $flag = "x";;
 	$flag = $_[0] if (defined $_[0]);
 	use POSIX qw(strftime);
-	my $now = POSIX::strftime("%Y-%m-%d.%H.%M", localtime());	
+	my $now = POSIX::strftime("%Y-%m-%d.%H.%M", localtime());
 	my $instlog = "PoolManager Install Log.\n$now\n";
 	print "Perl module check \n" if ($flag ne "-q");
 	require RRDs;
@@ -55,17 +55,17 @@ sub doInstall {
 	print " ..all set!\n" if ($flag ne "-q");
 	$instlog .= "Perl test passed.";
 
-# The following three values may need adjusting on systems that are not Debian or RedHat based. 
+# The following three values may need adjusting on systems that are not Debian or RedHat based.
 	my $webdir = "/var/www";
 	if (-d "/etc/lighttpd" && !-d "/var/www/cgi-bin" ) { `ln -s /usr/lib/cgi-bin /var/www/cgi-bin` }
-	my $cgidir = "/usr/lib/cgi-bin"; 
+	my $cgidir = "/usr/lib/cgi-bin";
   my $apacheuser = "unknown";
 
 	my $appdir = "/opt/ifmi";
   	$apacheuser = "apache" if (-f "/etc/redhat-release");
-  	$apacheuser = "www-data" if (-f "/etc/debian_version"); 
+  	$apacheuser = "www-data" if (-f "/etc/debian_version");
   	if ($apacheuser ne "unknown") {
-	    if (-d $webdir && -d $cgidir) { 
+	    if (-d $webdir && -d $cgidir) {
 			print "Copying files...\n" if ($flag ne "-q");
 			#perl chown requires UID and make_path is broken, so
 			make_path $appdir . '/rrdtool';
@@ -77,7 +77,7 @@ sub doInstall {
     	}
     	copy "index.html", $webdir;
     	if (!-f $cgidir . "/status.pl.pre-ifmi") {
-    		copy $cgidir . "/status.pl", $cgidir . "/status.pl.pre-ifmi" if (-f $cgidir . "/status.pl"); 			
+    		copy $cgidir . "/status.pl", $cgidir . "/status.pl.pre-ifmi" if (-f $cgidir . "/status.pl");
     	}
     	copy "status.pl", $cgidir;
 			copy "config.pl", $cgidir;
@@ -86,19 +86,19 @@ sub doInstall {
     	copy "favicon.ico", $webdir;
     	copy "mcontrol", $appdir;
     	copy "pm-common.pl", $appdir;
-  		copy "pmgraph.pl", $appdir; 
+  		copy "pmgraph.pl", $appdir;
   		copy "pmnotify.pl", $appdir;
       copy "run-poolmanager.pl", $appdir;
     	copy "sendstatus.pl", $appdir;
     	`cp themes/* $webdir/IFMI/themes`;
     	`cp images/*.png $webdir/IFMI`;
-    	`chmod 0755 $appdir/*.pl`; #because windows f's up the permissions. wtf. 
+    	`chmod 0755 $appdir/*.pl`; #because windows f's up the permissions. wtf.
     	`chmod 0755 $appdir/mcontrol`; #because windows
     	`chmod 0755 $appdir/farmview`; #because windows
     	`chmod 0755 $cgidir/*.pl`; #because windows
 			`chown $apacheuser $appdir/poolmanager.conf` if (-f "$appdir/poolmanager.conf");
     	$instlog .= "files copied.\n";
-		} else { 
+		} else {
 			die "Your web directories are in unexpected places. Quitting.\n";
 		}
 		copy "/etc/crontab", "/etc/crontab.pre-ifmi" if (!-f "/etc/crontab.pre-ifmi");
@@ -126,7 +126,7 @@ sub doInstall {
 			print "The next set of questions is information for this cert.\n" if ($flag ne "-q");
 			print "Please set the country code, and the rest of the cert quetions can be left blank.\n";
 			print "Press any key to continue: ";
-			my $creply = <STDIN>; 
+			my $creply = <STDIN>;
 			if ($creply =~ m/.*/) {
 			    `/usr/bin/openssl req -x509 -nodes -days 1825 -newkey rsa:2048 -keyout /etc/ssl/private/apache.key -out /etc/ssl/certs/apache.crt`;
 			    print "...finished creating cert.\n";
@@ -135,7 +135,7 @@ sub doInstall {
 	 	} else {
     		print "...cert appears to be installed, skipping...\n" if ($flag ne "-q");
 		}
-		
+
 		#Lighttpd additions begin
 		my $lrestart = 0;
 		if (-d "/etc/lighttpd") {
@@ -146,7 +146,7 @@ sub doInstall {
 				`sed -i 's/#       \"mod_rewrite\"/       \"mod_rewrite\"/g' /etc/lighttpd/lighttpd.conf`;
 				$lrestart++;
 			}
-			if (!-f "/etc/lighttpd/conf-enabled/10-ssl.conf") { 
+			if (!-f "/etc/lighttpd/conf-enabled/10-ssl.conf") {
 				`lighty-enable-mod ssl`;
 				copy "/etc/lighttpd/conf-available/10-ssl.conf", "/etc/lighttpd/conf-available/10-ssl.conf.pre-ifmi";
 				`sed -i "s/server.pem/snakeoil.pem/g" /etc/lighttpd/conf-available/10-ssl.conf`;
@@ -159,7 +159,7 @@ sub doInstall {
 			`service lighttpd restart` if ($lrestart > 0);
 			#Lighttpd additions end.
 		} else {
-			my $restart = 0; 
+			my $restart = 0;
    			copy "/etc/apache2/sites-available/default-ssl", "/etc/apache2/sites-available/default-ssl.pre-ifmi"
 			if (!-f "/etc/apache2/sites-available/default-ssl.pre-ifmi");
 	    	if (`grep ssl-cert-snakeoil.pem /etc/apache2/sites-available/default-ssl`) {
@@ -183,7 +183,7 @@ sub doInstall {
 			    }
 	    		close $dout;
 				move "/etc/apache2/sites-available/default-ssl.out", "/etc/apache2/sites-available/default-ssl";
-	 	 } 
+	 	 }
     	if (! `grep RewriteEngine /etc/apache2/sites-available/default`) {
 	    	copy "/etc/apache2/sites-available/default", "/etc/apache2/sites-available/default.pre-ifmi"
 	    		if (!-f "/etc/apache2/sites-available/default.pre-ifmi");
@@ -206,14 +206,16 @@ sub doInstall {
 			}
 			`service apache2 restart` if ($restart > 0);
 		}
+   my $rpcheck = `ps -eo command | grep -c [r]un-poolmanager.pl`;
+   `killall -9 run-poolmanager.pl` if ($rpcheck > 0);
 		print "Please read the README and edit your miner conf file as required.\nDone! Thank you for flying IFMI!\n" if ($flag ne "-q");
-	} else { 
+	} else {
 		print "Cant determine apache user, Bailing out!\n";
 		$instlog .= "unknown apache user, bailed out.\n";
 	}
 	open my $lin, '>', "PM-install-log.$now";
 	print $lin $instlog;
-	close $lin; 
+	close $lin;
 }
 
 
